0024e8ae6562b36123358192de16cc179da97a4c
FoxedDev <169158948+foxeddev@users.noreply.github.com>
2025-12-30 14:54:39 +0100

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.png';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>Beet</title>
+
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=DM+Mono:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import logo from '$lib/assets/beet.png';
+</script>
+
+<div
+	class="space-y-20 selection:bg-primary bg-radial-[at_50%_90%] from-accent to-75% to-background p-24 w-full min-h-screen"
+>
+	<div>
+		<h1 class="text-[12rem] text-foreground text-center leading-none tracking-wider">
+			Meet <span class="font-bold text-primary selection:text-accent">Beet</span>
+		</h1>
+		<h2 class="font-medium text-secondary text-5xl text-center tracking-wider">
+			The Minecraft pack development kit
+		</h2>
+	</div>
+
+	<div class="flex justify-center">
+		<div class="flex bg-background">
+			<input
+				type="text"
+				value="$ pip install beet"
+				readonly
+				class="px-8 py-4 outline-0 font-mono text-foreground text-2xl"
+			/>
+			<button
+				class="bg-primary px-8 py-4 font-sans text-foreground text-2xl hover:-translate-y-2 hover:translate-x-2 active:-translate-y-1 active:translate-x-1 duration-75 cursor-pointer"
+				>Copy</button
+			>
+		</div>
+	</div>
+
+	<img src={logo} alt="" class="bottom-0 left-1/2 fixed size-96 -translate-x-1/2" />
+</div>
