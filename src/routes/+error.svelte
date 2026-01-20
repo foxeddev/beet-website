77a9233080d8eeef6acac67821acf23ff15c54d6
FoxedDev <169158948+foxeddev@users.noreply.github.com>
2026-01-20 18:10:05 +0100

    Fix error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,30 +1,30 @@
 <script lang="ts">
-	import logo from "$lib/assets/beet.png"
+	import logo from '$lib/assets/beet.png';
 	import { page } from '$app/state';
 </script>
 
 <div
-	id="body"
-	class="selection:bg-primary bg-radial-[at_50vw_85vh] from-accent to-75% to-background"
+	id="wrapper"
+	class="space-y-32 selection:bg-primary bg-radial-[circle_at_50vw_85vh] from-accent to-[80vh] to-background"
 >
 	<header class="space-y-8 md:space-y-20 p-8 md:p-12 xl:p-20 w-full h-screen">
 		<div>
 			<h1
-				class="font-bold text-primary xl:text-[12rem] selection:text-accent text-9xl text-center leading-30 md:leading-none tracking-wider"
+				class="text-foreground xl:text-[12rem] text-9xl text-center leading-30 md:leading-none tracking-wider"
 			>
-				{page.status}
+				Error <span class="font-bold text-primary selection:text-accent">{page.status}</span>
 			</h1>
 			<h2 class="font-medium text-secondary text-2xl md:text-4xl text-center tracking-wider">
 				{page.error?.message}
 			</h2>
 		</div>
 
-		<div class="flex md:flex-row flex-col justify-center gap-2 md:gap-8 m-auto w-fit">
+		<div class="flex flex-row flex-wrap justify-center gap-2 md:gap-8 m-auto w-fit">
 			<a
-				class="flex justify-center items-center gap-1 md:gap-2 bg-primary px-4 md:px-8 py-3 md:py-4 font-sans text-foreground text-md md:text-2xl text-center hover:-translate-y-2 hover:translate-x-2 active:-translate-y-1 active:translate-x-1 duration-75 cursor-pointer"
+				class="flex justify-center items-center gap-1 md:gap-2 bg-primary px-4 md:px-8 py-3 md:py-4 font-sans text-foreground text-md md:text-2xl text-center transition hover:-translate-y-2 hover:translate-x-2 active:-translate-y-1 active:translate-x-1 cursor-pointer"
 				href="/"
 			>
-				Return to Homepage
+				Return To Homepage
 			</a>
 		</div>
 
