c83b992cb8e6f3c9e6b86aa83d79075b785e7d7f
FoxedDev <169158948+foxeddev@users.noreply.github.com>
2026-02-01 16:54:56 +0100

    Add open graph metadata

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,38 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.png';
+	import screenshot from '$lib/assets/screenshot.png';
+	import { page } from '$app/state';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
+	<!-- Meta -->
+
 	<link rel="icon" href={favicon} />
-	<title>Beet</title>
+	<title>Meet Beet</title>
+	<meta name="description" content="The Minecraft pack development kit" />
+
+	<!-- Open Graph Protocol -->
+
+	<meta property="og:title" content="Meet Beet" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content={page.url.toString()} />
+	<meta property="og:description" content="The Minecraft pack development kit" />
+	<meta property="og:image" content={screenshot} />
+	<meta property="og:image:secure_url" content={screenshot} />
+	<meta property="og:image:type" content="image/png" />
+	<meta property="og:image:width" content="1920" />
+	<meta property="og:image:height" content="1080" />
+	<meta
+		property="og:image:alt"
+		content={'A website with a big title and description and an image of a pixel art beetroot at the bottom.'}
+	/>
+	<meta property="og:locale" content="en_US" />
+	<meta property="og:site_name" content="Beet" />
+
+	<!-- Fonts -->
 
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
