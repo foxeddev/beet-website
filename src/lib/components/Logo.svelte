68319833d8eb4674db44a386eed05c155da5e4aa
FoxedDev <169158948+foxeddev@users.noreply.github.com>
2026-06-07 20:10:26 +0200

    Make the logo a link

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -6,8 +6,10 @@
 
 <svelte:window bind:scrollY />
 
-<img
-	src={logo}
-	alt="Beet Logo"
-	class={`fixed bottom-0 left-1/2 z-10 -translate-x-1/2 drop-shadow-2xl drop-shadow-glow/50 duration-200 ${scrollY < 200 ? 'h-[calc(100vh-32rem)] max-h-96' : 'pointer-events-none h-32 md:h-48'}`}
-/>
+<a href="#top">
+	<img
+		src={logo}
+		alt="Beet Logo"
+		class={`fixed bottom-0 left-1/2 z-10 -translate-x-1/2 drop-shadow-2xl drop-shadow-glow/50 duration-200 ${scrollY < 200 ? 'h-[calc(100vh-32rem)] max-h-96' : 'h-32 md:h-48'}`}
+	/>
+</a>
