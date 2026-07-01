360843fce35d328376389556d0ba06067428a113
FoxedDev <169158948+foxeddev@users.noreply.github.com>
2026-07-01 07:13:04 +0200

    Add fetchpriority="high" to logo

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -10,6 +10,7 @@
 	<img
 		src={logo}
 		alt="Beet Logo"
+  fetchpriority="high"
 		class={`fixed bottom-0 left-1/2 z-10 -translate-x-1/2 drop-shadow-2xl drop-shadow-glow/50 duration-200 ${scrollY < 200 ? 'h-[calc(100vh-32rem)] max-h-96' : 'h-32 md:h-48'}`}
 	/>
 </a>
